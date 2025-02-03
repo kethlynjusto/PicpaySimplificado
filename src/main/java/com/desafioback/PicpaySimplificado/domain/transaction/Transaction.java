c30d19252517f2e1a4961aee69129dfe721a53ec
Kethlyn Justo <kethdiniz06@gmail.com>
2025-02-03 14:30:31 -0300

    teste

--- src/main/java/com/desafioback/PicpaySimplificado/domain/transaction/Transaction.java
+++ src/main/java/com/desafioback/PicpaySimplificado/domain/transaction/Transaction.java
@@ -1,4 +1,32 @@
 package com.desafioback.PicpaySimplificado.domain.transaction;
 
+import com.desafioback.PicpaySimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
 }
--- src/main/java/com/desafioback/PicpaySimplificado/repositories/UserRepository.java
+++ src/main/java/com/desafioback/PicpaySimplificado/repositories/UserRepository.java
@@ -1,4 +1,12 @@
 package com.desafioback.PicpaySimplificado.repositories;
 
-public class UserRepository {
+import com.desafioback.PicpaySimplificado.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
 }
